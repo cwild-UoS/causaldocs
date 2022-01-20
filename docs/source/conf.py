27729a4e20da3f6868e1dd23c1bceec2a8f63537
cwild-UoS <93984046+cwild-UoS@users.noreply.github.com>
2022-01-20 10:52:19 +0000

    Add generation to sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 #sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "causal_testing", "data_collection")))
 #sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "causal_testing", "generation")))
 #sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "causal_testing", "specification")))
