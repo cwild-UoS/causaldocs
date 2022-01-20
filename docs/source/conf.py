a201cb9be2344aa8dab32151c944b74c322f2790
cwild-UoS <93984046+cwild-UoS@users.noreply.github.com>
2022-01-20 10:33:06 +0000

    Add generation to sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "causal_testing", "data_collection")))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "causal_testing", "generation")))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "causal_testing", "specification")))
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "causal_testing", "data_collection")))
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "causal_testing", "generation")))
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "causal_testing", "specification")))
 print("hello")
 print(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "causal_testing", "generation")))
 # -- Project information -----------------------------------------------------
